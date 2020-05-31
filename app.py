01d666cfa8b90614e197c7aa06e7e8183ad97a99
shiwangi20 <gargshiwangi2703@gmail.com>
2020-05-31 17:30:30 +0530

    added login form

--- app.py
+++ app.py
@@ -47,9 +47,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You are logged in successfully!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessfull!! Check your email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
